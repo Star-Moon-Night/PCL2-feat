d6a283093decf62b67baa283fe76f11728fe9b96
Star-Moon-Night <80152431+Star-Moon-Night@users.noreply.github.com>
2025-02-16 23:16:58 +0800

    Update ModSecret.vb

--- Plain Craft Launcher 2/Modules/ModSecret.vb
+++ Plain Craft Launcher 2/Modules/ModSecret.vb
@@ -11,7 +11,7 @@ Friend Module ModSecret
     '在开源版的注册表与常规版的注册表隔离，以防数据冲突
     Public Const RegFolder As String = "PCLDebug"
     '用于微软登录的 ClientId
-    Public Const OAuthClientId As String = ""
+    Public Const OAuthClientId As String = "3ffa3597-3116-4daf-8dd3-0cc13364f026"
     'CurseForge API Key
     Public Const CurseForgeAPIKey As String = ""
 
